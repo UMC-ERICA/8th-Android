12562e4ed4a93c94b2760fdb84f655ad8a4ad1f6
Jhyun02 <hjh75035117@gmail.com>
2025-03-22 22:54:32 +0900

    feat: 1주차 실전미션

--- app/src/main/java/com/example/week1/nextActivity.kt
+++ app/src/main/java/com/example/week1/nextActivity.kt
@@ -1,29 +1,20 @@
 package com.example.week1
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class nextActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_next)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        val button : ImageButton = findViewById(R.id.yellowBtn)
-        button.setOnClickListener{
-            val intent = Intent(this, nextActivity::class.java)
-            startActivity(intent)
-        }
-
     }
 }
