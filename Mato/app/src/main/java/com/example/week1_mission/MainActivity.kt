f72c213231d029b5ae29d002f0f5e304ca350ea4
Jhyun02 <hjh75035117@gmail.com>
2025-03-25 22:28:18 +0900

    refactor: viewBining 사용

--- Mato/app/src/main/java/com/example/week1_mission/MainActivity.kt
+++ Mato/app/src/main/java/com/example/week1_mission/MainActivity.kt
@@ -2,25 +2,27 @@ package com.example.week1_mission
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.week1_mission.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
-        val button : ImageButton = findViewById(R.id.yellowBtn)
-        button.setOnClickListener{
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.yellowBtn.setOnClickListener{
             val intent = Intent(this, nextActivity::class.java)
             startActivity(intent)
         }
