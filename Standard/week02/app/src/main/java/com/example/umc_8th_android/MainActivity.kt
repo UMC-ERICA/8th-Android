7084e91b82bf6882d783f16a3d1552982906c7a8
장기준 <autojjangs@naver.com>
2025-03-30 18:44:18 +0900

    feat: homeFragment를 startDestination으로 설정

--- Standard/week02/app/src/main/java/com/example/umc_8th_android/MainActivity.kt
+++ Standard/week02/app/src/main/java/com/example/umc_8th_android/MainActivity.kt
@@ -3,11 +3,12 @@ package com.example.umc_8th_android
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.umc_8th_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
 
     private lateinit var binding: ActivityMainBinding
 
@@ -18,6 +19,40 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
 
+        val navController = navHostFragment.navController
+
+        // BottomNavigationView 설정
+        binding.navBar.setupWithNavController(navController)
+
+        // BottomNavigationView 아이템 선택 리스너 설정
+        binding.navBar.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home -> {
+                    navController.navigate(R.id.fragment_home)
+                    true
+                }
+                R.id.map -> {
+                    navController.navigate(R.id.fragment_map)
+                    true
+                }
+
+                R.id.chat -> {
+                    navController.navigate(R.id.fragment_chat)
+                    true
+                }
+
+                R.id.mypage -> {
+                    navController.navigate(R.id.fragment_mypage)
+                    true
+                }
+
+
+                else -> false
+            }
+
+        }
     }
 }
