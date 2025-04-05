be972ab20b0969055b90446650c5bd4d8b655c43
Jhyun02 <hjh75035117@gmail.com>
2025-04-05 23:43:52 +0900

    feat: fragment 전환 추가

--- Mato/flo/app/src/main/java/com/example/umc_flo/AlbumFragment.kt
+++ Mato/flo/app/src/main/java/com/example/umc_flo/AlbumFragment.kt
@@ -5,7 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.umc_flo.databinding.FragmentHomeBinding
+import com.example.umc_flo.databinding.FragmentAlbumBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -14,12 +14,12 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
+ * Use the [AlbumFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class HomeFragment : Fragment() {
+class AlbumFragment : Fragment() {
+    lateinit var binding: FragmentAlbumBinding
 
-    lateinit var binding: FragmentHomeBinding
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -36,12 +36,11 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        binding = FragmentAlbumBinding.inflate(inflater,container,false)
 
-        binding.homePannelAlbumImg01Iv.setOnClickListener{
-            (context as MainActivity).supportFragmentManager.beginTransaction().replace(R.id.main_frm,AlbumFragment()).commitAllowingStateLoss()
+        binding.albumBackIv.setOnClickListener {
+            (context as MainActivity).supportFragmentManager.beginTransaction().replace(R.id.main_frm,HomeFragment()).commitAllowingStateLoss()
         }
-        // Inflate the layout for this fragment
         return binding.root
     }
 
@@ -52,12 +51,12 @@ class HomeFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment homeFragment.
+         * @return A new instance of fragment AlbumFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
+            AlbumFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
