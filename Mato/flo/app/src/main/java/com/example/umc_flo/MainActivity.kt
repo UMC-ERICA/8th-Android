617a6ee885532adb7628b61f475683d6d870b299
Jhyun02 <hjh75035117@gmail.com>
2025-04-05 20:30:03 +0900

    feat: activity 전환 구현

---
+++ Mato/flo/app/src/main/java/com/example/umc_flo/MainActivity.kt
@@ -0,0 +1,73 @@
+    package com.example.umc_flo
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.umc_flo.databinding.ActivityMainBinding
+
+    class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.mainPlayerCl.setOnClickListener {
+            startActivity(Intent(this, SongActivity::class.java))
+        }
+
+        initBottomNavigation()
+    }
+
+        private fun initBottomNavigation(){
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.main_frm, HomeFragment())
+                .commitAllowingStateLoss()
+
+            binding.mainBnv.setOnItemSelectedListener{ item ->
+                when (item.itemId) {
+
+                    R.id.homeFragment -> {
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.main_frm, HomeFragment())
+                            .commitAllowingStateLoss()
+                        return@setOnItemSelectedListener true
+                    }
+
+                    R.id.lookFragment -> {
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.main_frm, LookFragment())
+                            .commitAllowingStateLoss()
+                        return@setOnItemSelectedListener true
+                    }
+                    R.id.searchFragment -> {
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.main_frm, SearchFragment())
+                            .commitAllowingStateLoss()
+                        return@setOnItemSelectedListener true
+                    }
+                    R.id.lockerFragment -> {
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.main_frm, LockerFragment())
+                            .commitAllowingStateLoss()
+                        return@setOnItemSelectedListener true
+                    }
+                }
+                false
+            }
+        }
+
+}
---
+++ Mato/flo/app/src/main/java/com/example/umc_flo/SongActivity.kt
@@ -0,0 +1,30 @@
+package com.example.umc_flo
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.umc_flo.databinding.ActivitySongBinding
+
+class SongActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivitySongBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivitySongBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.songDownIb.setOnClickListener {
+            finish()
+        }
+    }
+}
