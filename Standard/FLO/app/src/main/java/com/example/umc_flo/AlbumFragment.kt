cf1d413bc689ccdeb94e8bef5d647f63735501e5
장기준 <autojjangs@naver.com>
2025-04-12 20:49:49 +0900

    feat: AlbumFragment에서 뒤로가기 시, HomeFragment가 나오게 설정

--- Standard/FLO/app/src/main/java/com/example/umc_flo/AlbumFragment.kt
+++ Standard/FLO/app/src/main/java/com/example/umc_flo/AlbumFragment.kt
@@ -1,15 +1,24 @@
 package com.example.umc_flo
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import com.example.umc_flo.databinding.FragmentAlbumBinding
 import com.google.android.material.tabs.TabLayoutMediator
 
 class AlbumFragment : Fragment() {
     lateinit var binding : FragmentAlbumBinding
+
+    private val information = arrayListOf("수록곡", "상세정보", "영상")
+
+    private lateinit var callback: OnBackPressedCallback
+
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,10 +36,29 @@ class AlbumFragment : Fragment() {
         val albumAdapter = AlbumVPAdapter(this)
         binding.albumContentVp.adapter = albumAdapter
         TabLayoutMediator(binding.albumContentTb, binding.albumContentVp) {
-            tab, position
-        }
-
+            tab, position ->
+            tab.text = information[position]
+        }.attach()
 
         return binding.root
+
+    }
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                requireActivity().supportFragmentManager.beginTransaction()
+                    .replace(R.id.main_fragment_container, HomeFragment())
+                    .commit()
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(this, callback)
+
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        callback.remove()
     }
 }
