e68b64a0f1e5837b2aa44ce0cf31614a74b76d1c
장기준 <autojjangs@naver.com>
2025-05-08 14:44:35 +0900

    feat: AlbumRVAdapter.kt 코드 작성

---
+++ Standard/FLO/app/src/main/java/com/example/umc_flo/AlbumRVAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.umc_flo
+
+import android.text.Layout
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.umc_flo.databinding.ItemAlbumBinding
+
+class AlbumRVAdapter(private val albumList: ArrayList<Album>): RecyclerView.Adapter<AlbumRVAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AlbumRVAdapter.ViewHolder {
+        val binding: ItemAlbumBinding = ItemAlbumBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: AlbumRVAdapter.ViewHolder, position: Int) {
+        holder.bind(albumList[position]) }
+
+
+    override fun getItemCount(): Int = albumList.size
+
+    inner class ViewHolder(val binding: ItemAlbumBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(album: Album) {
+            binding.itemAlbumTitleTv.text = album.title
+            binding.itemAlbumSingerTv.text = album.singer
+            binding.itemAlbumCoverImgIv.setImageResource(album.coverImg!!)
+        }
+    }
+
+}
--- Standard/FLO/app/src/main/java/com/example/umc_flo/HomeFragment.kt
+++ Standard/FLO/app/src/main/java/com/example/umc_flo/HomeFragment.kt
@@ -13,6 +13,7 @@ import com.example.umc_flo.databinding.FragmentHomeBinding
 class HomeFragment :Fragment() {
     lateinit var binding: FragmentHomeBinding
     lateinit var adapter: BannerVPAdapter
+    private var albumDatas = ArrayList<Album>()
 
     val slideDelay = 2500
     val sliderHandler = Handler(Looper.getMainLooper())
@@ -28,13 +29,22 @@ class HomeFragment :Fragment() {
     override fun onCreateView(inflater: LayoutInflater,
                           container: ViewGroup?,
                           savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
 
-        binding.homeAlbumImg1Iv.setOnClickListener {
-            (context as MainActivity).supportFragmentManager.beginTransaction()
-                .replace(R.id.main_fragment_container, AlbumFragment())
-                .commitAllowingStateLoss()
+//        binding.homeAlbumImg1Iv.setOnClickListener {
+//            (context as MainActivity).supportFragmentManager.beginTransaction()
+//                .replace(R.id.main_fragment_container, AlbumFragment())
+//                .commitAllowingStateLoss()
+//        }
+        albumDatas.apply {
+            add(Album("Butter","방탄소년단(BTS)", R.drawable.img_album_exp))
+            add(Album("LILAC","아이유(IU)", R.drawable.img_album_exp2))
+            add(Album("Next Level","에스파(AESPA)", R.drawable.img_album_exp3))
+            add(Album("Boy with Luv","방탄소년단(BTS)", R.drawable.img_album_exp4))
+            add(Album("BBoom BBoom","모모랜드(MOMOLAND)", R.drawable.img_album_exp5))
+            add(Album("Weekend","태연(Tae Yeon", R.drawable.img_album_exp6))
+
         }
 
         val bannerAdapter = BannerVPAdapter(this)
