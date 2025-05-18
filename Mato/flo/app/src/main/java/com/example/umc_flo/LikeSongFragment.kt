bae8a0822bf8d59b90155aba926a8720be41d910
Jhyun02 <hjh75035117@gmail.com>
2025-05-18 18:13:17 +0900

    feat:보관함 좋아요 기능 구현

---
+++ Mato/flo/app/src/main/java/com/example/umc_flo/LikeSongFragment.kt
@@ -0,0 +1,57 @@
+package com.example.umc_flo
+
+import LikedSongRVAdapter
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.umc_flo.databinding.FragmentLikeSongBinding
+
+class LikeSongFragment: Fragment() {
+    private lateinit var binding: FragmentLikeSongBinding
+    private lateinit var songDB: SongDatabase
+    private lateinit var adapter: LikedSongRVAdapter
+    private val songs = ArrayList<Song>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentLikeSongBinding.inflate(inflater, container, false)
+        songDB = SongDatabase.getInstance(requireContext())!!
+
+        loadLikedSongs()
+
+        return binding.root
+    }
+
+    private fun loadLikedSongs() {
+        val likedSongs = ArrayList(songDB.songDao().getLikedSongs(true))
+
+        adapter = LikedSongRVAdapter(
+            likedSongs,
+            onClickSong = { song ->
+                val intent = Intent(context, SongActivity::class.java)
+                val editor = requireContext().getSharedPreferences("song", AppCompatActivity.MODE_PRIVATE).edit()
+                editor.putInt("songId", song.id)
+                editor.apply()
+                startActivity(intent)
+            },
+            onToggleLike = { song ->
+                songDB.songDao().updateIsLikeById(false, song.id)
+            }
+        )
+
+        binding.likeSongRv.layoutManager = LinearLayoutManager(context)
+        binding.likeSongRv.adapter = adapter
+    }
+
+    override fun onResume() {
+        super.onResume()
+        loadLikedSongs() // 다시 돌아왔을 때 새로고침
+    }
+}
